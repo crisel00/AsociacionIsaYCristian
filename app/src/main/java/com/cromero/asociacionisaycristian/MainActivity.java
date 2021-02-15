107c85a9f926bd5129b97108c7e2c3553ed91a69
crisel00 <croro.abadia@gmail.com>
2021-02-15 12:34:52 +0100

    prueba 2

--- app/src/main/java/com/cromero/asociacionisaycristian/MainActivity.java
+++ app/src/main/java/com/cromero/asociacionisaycristian/MainActivity.java
@@ -9,8 +9,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //comentario random
         setContentView(R.layout.activity_main);
-        //hola isa
-        //hola cristian
+        //comentario random
+        soyCristian();
+    }
+    //comentario random
+    public void soyCristian(){
+        System.out.println("Hola, soy Cristian n-n");
+        //comentario random
+        //comentario random
+        //comentario random
+        //comentario random
     }
 }
