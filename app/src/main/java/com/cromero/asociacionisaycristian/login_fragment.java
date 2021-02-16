919d781b813b8efb077f1694d246ace83fccce42
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2021-02-16 13:36:33 +0100

    Login lanza TabbedActivity

--- app/src/main/java/com/cromero/asociacionisaycristian/login_fragment.java
+++ app/src/main/java/com/cromero/asociacionisaycristian/login_fragment.java
@@ -1,5 +1,6 @@
 package com.cromero.asociacionisaycristian;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -24,15 +25,6 @@ import com.google.firebase.auth.FirebaseAuth;
  * create an instance of this fragment.
  */
 public class login_fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     Button bt_login, bt_registrar;
     EditText et_correo, et_contrasena;
 
@@ -40,21 +32,9 @@ public class login_fragment extends Fragment {
     public login_fragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment login_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static login_fragment newInstance(String param1, String param2) {
         login_fragment fragment = new login_fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,10 +42,6 @@ public class login_fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -90,7 +66,7 @@ public class login_fragment extends Fragment {
             public void onClick(View view) {
                 String correo = et_correo.getText().toString();
                 String contrasena = et_contrasena.getText().toString();
-                if(correo.length()<5 || contrasena.length()<5){
+                if(correo.length()<5 || contrasena.length()<6){
 
                 } else {
                     iniciaSesion(correo,contrasena);
@@ -114,6 +90,7 @@ public class login_fragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         exito = task.isSuccessful();
+                        startActivity(new Intent(getContext(),TabbedActivity.class));
                     }
                 }
         );
