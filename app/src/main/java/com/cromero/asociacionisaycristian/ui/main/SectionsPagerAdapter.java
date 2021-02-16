681e611726f63306999e7db39ec8fda37ced179f
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2021-02-16 12:46:16 +0100

    TabbedActivity

---
+++ app/src/main/java/com/cromero/asociacionisaycristian/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.cromero.asociacionisaycristian.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.cromero.asociacionisaycristian.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CharSequence title=null;
+        switch (position){
+            case 1:
+                title= "SECTION 1";
+                break;
+            case 2:
+                title=  "SECTION 2";
+            break;
+            case 3:
+                title=  "SECTION 3";
+            break;
+        }
+        return title;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
